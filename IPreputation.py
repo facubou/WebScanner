f0770ea9e719d34e7d696717ff5f7e8e29e62c89
Facundo Bouza <facundo.bouza@gmail.com>
2019-12-04 14:43:14 -0300

    percent completed added

--- IPreputation.py
+++ IPreputation.py
@@ -4,11 +4,19 @@ import re
 salida = ""
 entrada = open("bulk.txt", "r")
 export = ""
+contador = 0
+conteoTotal = 0
+
+for j in entrada:
+  conteoTotal = conteoTotal + 1
+
+print(conteoTotal)
+print(entrada)
 
 for i in entrada:
   dominio = 'https://www.abuseipdb.com/check/' + i
   dominio = dominio.rstrip('\n')
-  #print (dominio)
+  print (dominio)
   webResponse = requests.get(dominio, timeout=15)
   contenido = BeautifulSoup(webResponse.content, "html.parser")
   contenido = str(contenido).replace("\n", "")
@@ -22,8 +30,10 @@ for i in entrada:
   salida = (str(i) + "," + str(porcentajeRiego) + "," + str(cantidadReportes) + "," + str(hostname) + "," + str(dominio) + "," + str(pais)) 
   salida = salida.replace('\n', "")
   export = export + salida + "\n"
+  contador = contador + 1
+  print ("Completado: " + str(contador) + " de " + str(conteoTotal))
 
 
-w = open("salida.txt", "w")
-w.write(str(export))
-w.close()
+salida3 = open("salida.txt", "w")
+salida3.write(str(export))
+salida3.close()
